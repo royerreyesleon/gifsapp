ad9753b03cb917be6a5619f2054d1df0b3df8f65
Royer Reyes León <royerleon7@gmail.com>
2022-06-09 12:55:27 -0500

    get http rest api

--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   styleUrls: ['./resultados.component.css']
 })
-export class ResultadosComponent implements OnInit {
+export class ResultadosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    get resultados() {
+        return this.gifsService.resultados;
+    }
+  constructor(private gifsService: GifsService) { }
 
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,27 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Photo, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-    
-    private _historial: string[] = [];
-    
-    get historial() {
-        return [...this._historial];
-    }
-    
-    buscarGifs(query: string = '') {
-        
-        query = query.trim().toLocaleLowerCase();
+  /* https://www.pexels.com/es-es/api/documentation/ */
+  /* https://app.quicktype.io/ */
+  private _apiKey: string =
+    '563492ad6f91700001000001b1003998affb475ca89193cc5e39595b';
 
-        if (!this._historial.includes(query)) {
-            this._historial.unshift(query);
-            this._historial = this._historial.splice(0,10)
-            console.log(this._historial );
-        }
-        
+  private _historial: string[] = [];
+
+  public resultados: Photo[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {}
+
+  buscarGifs(query: string = '') {
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+      console.log(this._historial);
     }
 
-  constructor() { }
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: this._apiKey,
+      }),
+    };
+    this.http
+      .get<SearchGifsResponse>(
+        `https://api.pexels.com/v1/search?query=${query}&per_page=10`,
+        httpOptions
+      )
+      /*.subscribe((resp: any) => {*/
+      .subscribe((resp) => {
+        console.log(resp.photos);
+        this.resultados = resp.photos;
+      });
+  }
 }
