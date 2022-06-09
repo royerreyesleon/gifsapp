1958a2e28ed6a7572652523dc378e9675df037d2
Royer Reyes León <royerleon7@gmail.com>
2022-06-09 10:48:55 -0500

    add array string to service

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,6 +9,9 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent {
     
     @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+    
+    constructor(private gifsService: GifsService) {
+    }
     
     //buscar( event: any) {
     /*buscar( event: KeyboardEvent) {
@@ -21,6 +25,12 @@ export class BusquedaComponent {
     buscar() {
         const valor = this.txtBuscar.nativeElement.value;
         console.log(valor);
+        
+        if (valor.trim().length === 0) {
+            return;
+        }
+        
+        this.gifsService.buscarGifs(valor);
         
         this.txtBuscar.nativeElement.value = '';
     }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+    
+    get historial(): string[] {
+        return this.gifsService.historial;
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    constructor(private gifsService: GifsService) { }
 }
