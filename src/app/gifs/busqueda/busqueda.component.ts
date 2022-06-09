876ecfdb94b9139cccecc27e4dc48e1533ddc1a4
Royer Reyes León <royerleon7@gmail.com>
2022-06-09 10:32:27 -0500

    event enter input search

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class BusquedaComponent {
+    
+    @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+    
+    //buscar( event: any) {
+    /*buscar( event: KeyboardEvent) {
+        console.log(event);
+    }*/
+    
+    /*buscar(termino: string) {
+        console.log(termino);
+    }*/
+    
+    buscar() {
+        const valor = this.txtBuscar.nativeElement.value;
+        console.log(valor);
+        
+        this.txtBuscar.nativeElement.value = '';
+    }
 
 }
